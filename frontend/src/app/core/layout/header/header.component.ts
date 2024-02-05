685cd5f133f84305694f60ef7502f818be9949a6
Julien Von Der Marck <vdmjulien05@gmail.com>
2024-02-05 20:00:37 +0100

    feat(header): implement responsive navbar

--- frontend/src/app/core/layout/header/header.component.ts
+++ frontend/src/app/core/layout/header/header.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import {TuiDataListWrapperModule, TuiInputModule, TuiSelectModule} from "@taiga-ui/kit";
+import {TuiPrimitiveTextfieldModule, TuiTextfieldControllerModule} from "@taiga-ui/core";
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    TuiInputModule,
+    TuiTextfieldControllerModule,
+    TuiSelectModule,
+    TuiDataListWrapperModule,
+    ReactiveFormsModule,
+    TuiPrimitiveTextfieldModule,
+  ],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  readonly languages = [
+    'IT',
+    'EN',
+    'FR',
+  ];
+  formLanguage = new FormControl();
+
+  constructor() {
+    this.formLanguage.setValue('EN');
+  }
 
 }
