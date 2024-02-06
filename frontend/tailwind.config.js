bdbfb16bebfceecca816f80a873ec73b950bcd86
Julien Von Der Marck <vdmjulien@gmail.com>
2024-02-06 15:00:51 +0100

    feat(home): add main title hero page and card categories

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'italiana': ['Italiana', 'serif'],
+        'inter': ['Inter', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
