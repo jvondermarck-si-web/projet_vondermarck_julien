cd20de834208579bcfd3a167467625f667892548
Julien Von Der Marck <vdmjulien05@gmail.com>
2024-02-14 15:23:18 +0100

    :sparkles: refactor(footer): add new implementation of footer and also resize the w/h of the add to cart image on products

--- frontend/src/app/core/layout/footer/footer.component.ts
+++ frontend/src/app/core/layout/footer/footer.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {TuiSvgModule} from "@taiga-ui/core";
+import {TranslocoPipe} from "@ngneat/transloco";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    TuiSvgModule,
+    TranslocoPipe
+  ],
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
