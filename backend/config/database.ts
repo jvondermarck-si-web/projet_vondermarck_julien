deb8382e3536dc8d059104155bb60fda0d4eb144
Julien Von Der Marck <vdmjulien05@gmail.com>
2024-07-28 20:43:02 +0200

    feat: update database configuration to use PostgreSQL

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,37 +1,38 @@
 import { defineConfig } from '@adonisjs/lucid'
+import env from '../start/env.js'
 
 const dbConfig = defineConfig({
-  connection: 'sqlite', 
-  // connection: 'postgres',
+  //connection: 'sqlite', 
+  connection: 'postgres',
   connections: {
-    sqlite: {
-      client: 'sqlite',
-      connection: {
-        filename: "database.sqlite"
-      },
-      useNullAsDefault: true,
-      migrations: {
-        naturalSort: true,
-        paths: ['database/migrations'],
-      },
-      debug: true,
-    },
-    //postgres: {
-    //  client: 'postgres',
+    //sqlite: {
+    //  client: 'sqlite',
     //  connection: {
-    //    host: env.get('DB_HOST'),
-    //    port: env.get('DB_PORT'),
-    //    user: env.get('DB_USER'),
-    //    password: env.get('DB_PASSWORD'),
-    //    database: env.get('DB_DATABASE'),
-    //    ssl: env.get('DB_SSL'),
+    //    filename: "database.sqlite"
     //  },
+    //  useNullAsDefault: true,
     //  migrations: {
     //    naturalSort: true,
     //    paths: ['database/migrations'],
     //  },
     //  debug: true,
     //},
+    postgres: {
+      client: 'postgres',
+      connection: {
+        host: env.get('DB_HOST'),
+        port: env.get('DB_PORT'),
+        user: env.get('DB_USER'),
+        password: env.get('DB_PASSWORD'),
+        database: env.get('DB_DATABASE'),
+        ssl: env.get('DB_SSL'),
+      },
+      migrations: {
+        naturalSort: true,
+        paths: ['database/migrations'],
+      },
+      debug: true,
+    },
   },
 })
 
